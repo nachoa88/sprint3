d1c3d97fe794df5c3a0a3023630be1be1bc398f0
Nacho Albiol <nacho.albiol88@gmail.com>
2024-04-12 18:04:41 +0200

    Updated developers project

--- tema3-developers-demo/app/models/Task.php
+++ tema3-developers-demo/app/models/Task.php
@@ -1,61 +1,88 @@
 <?php
 
-enum Status
-{
-    case Pending;
-    case InProgress;
-    case Finished;
-}
-
 class Task
 {
-    // properties that represent the data 
-    private int $id;
-    private string $name;
-    private Status $status;
-    private string $dateTimeStarted;
-    private string $dateTimeFinished;
-    private string $user;
-
     public function __construct()
     {
-
     }
 
-    public function getConnection()
+    public function getData()
     {
         $filename = '../web/db/tasks.json';
         $data = file_get_contents($filename); //data read from json file
         return $data;
     }
 
-    // CRUD
+    // CRUD Logic
     public function getAllTasks(): array
     {
         // Implement the logic to get all tasks
-        $db = $this->getConnection();
-        $result = json_decode($db); //decode data
-        $tasks = $result;
+        $data = $this->getData();
+        $tasks = json_decode($data, true); //decode data to an array
 
         return $tasks;
     }
 
-    public function getTaskById()
+    public function getTaskById(int $id)
     {
-        // Implement the logic to get a task by its ID
-        return "Task found";
+        // Read tasks from JSON file
+        $data = $this->getData();
+        $tasks = json_decode($data, true);
+
+        // Find task with given ID
+        foreach ($tasks as $task) {
+            if ($task['id'] === $id) {
+                // If found, return and exits function. (return always exists the current function)
+                return $task;
+            }
+        }
+
+        // Return null if task not found
+        return null;
     }
 
     public function createTask()
     {
-        // Implement the logic to create a new task in the database
-        return "Task created";
+        $data = $this->getData();
+        $tasks = json_decode($data, true); //decode data to an array
+
+        // Get last id
+        $last_item = end($tasks);
+        $last_item_id = $last_item['id'];
+
+        // update id to last id + 1
+        $_POST['id'] = ++$last_item_id;
+
+        // add $_POST to $task array
+        $tasks[] = $_POST;
+        $jsonString = json_encode($tasks, JSON_PRETTY_PRINT);
+
+        // write to file
+        file_put_contents('../web/db/tasks.json', $jsonString, LOCK_EX);
     }
 
-    public function updateTask()
+    public function updateTask(int $id, array $newData)
     {
-        // Implement the logic to update a task in the database
-        return "Task updated";
+        // Read tasks from JSON file
+        $data = $this->getData();
+        $tasks = json_decode($data, true);
+
+        // Find task with given ID and update its data
+        $updatedTask = null;
+        // In PHP, foreach operates on a copy of the array, so we need to use 
+        // a reference to update the original array, this is done by using the & operator.
+        foreach ($tasks as &$task) {
+            if ($task['id'] === $id) {
+                $task = array_merge($task, $newData);
+                $updatedTask = $task;
+                break;
+            }
+        }
+
+        // Write tasks back to JSON file
+        file_put_contents('../web/db/tasks.json', json_encode($tasks), JSON_PRETTY_PRINT);
+
+        return $updatedTask;
     }
 
     public function deleteTask()
